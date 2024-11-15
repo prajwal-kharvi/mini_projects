c4a6aa01b24c1b710b7e353b96186f5ca42d0b84
prajwal_kharvi <kharviprajwalp@gmail.com>
2024-11-15 16:49:02 +0530

    organis file

--- api_data_to_file/api_data_to_file.js
+++ api_data_to_file/api_data_to_file.js
@@ -1,9 +1,12 @@
-
+import {url} from "./settings.js"
 
 async function api_data() {
-        const api=fetch("https://reqres.in/api/users")
+        const api=fetch(url)
         const response=await api
         const result=await response.json()
+        if(result.pagination.items?.count<=0){
+            console.log("not found 404")
+        }else{
         const jsonString = JSON.stringify(result, null, 2);
         const blob = new Blob([jsonString], { type: 'application/json',name:"jhsbs" });
         const link = document.createElement('a');
@@ -11,6 +14,7 @@ async function api_data() {
             link.download = 'data.json';
             link.click();
              URL.revokeObjectURL(link.href);
+        }
 }
 
 api_data()
